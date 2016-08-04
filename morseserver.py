22cf3138d300b417c4642ee41466c4eaf42b5702
TJ Hoff <theodore.hoff@gmail.com>
2016-08-04 17:42:28 -0600

    lies and calamity2

--- morseserver.py
+++ morseserver.py
@@ -13,6 +13,7 @@ TCP_PORT = 5005
 BUFFER_SIZE = 20  # Normally 1024, but we want fast response
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
 s.bind((TCP_IP, TCP_PORT))
 
 s.listen(1)
@@ -74,7 +75,7 @@ class Client:
 
     def send_message(self, message):
         try:
-            self.sockock.acquire()
+            self.socklock.acquire()
             self.conn.send(message.to_bytes())
             self.socklock.release()
         except socket.error:
